ce1e66a90bd7aa2a6d4aa43a387b63b2701559d9
Galatory <20934939+Galatory@users.noreply.github.com>
2019-06-17 18:50:55 +0200

    JSONfil er done!

--- Firebase/Kurser/src/main/java/ExportDataToJson.java
+++ Firebase/Kurser/src/main/java/ExportDataToJson.java
@@ -1,44 +1,58 @@
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import java.io.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import org.w3c.dom.*;
 import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExportDataToJson {
-    public static int PRETTY_PRINT_INDENT_FACTOR = 4;
-    public static String TEST_XML_STRING = "place";
     private static String USER_AGENT = "Mozilla/5.0";
+    private static String filePath = "database.json";
 
     public static void main(String argv[]) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new FileReader("rensetkurser.csv"));
-        ArrayList<String> id = new ArrayList<>();
-        while (reader.readLine() != null) {
-            id.add(reader.readLine().substring(0, 5));
-        }
+        // Array of all the courses as JSONObjects
+        JSONArray courses = new JSONArray();
 
-        Document doc;
-        for (String s : id) {
-            doc = loadXMLFromString(getCourse(s));
+        // The root object of the JSON file
+        JSONObject data = new JSONObject();
+
+        // Reads courses from "rensetkurser.csv"
+        BufferedReader reader = new BufferedReader(new FileReader("rensetkurser.csv"));
+        while (reader.readLine() != null) {
+            // Get the course code of the current course
+            String id = reader.readLine().substring(0, 5);
+
+            // Get the API result of the course in XML format
+            Document doc = loadXMLFromString(getCourse(id));
             doc.getDocumentElement().normalize();
 
+            // Generate a JSONObject for the current course
             NodeList nList = doc.getElementsByTagName("Course");
-            System.out.println(createJSONObject(nList));
+            JSONObject course = createJSONObject(nList);
+
+            // Add the object to courses if not null
+            if (!(course.get("courseCode") == null)) courses.add(course);
         }
+
+        // Put the list of JSONObject courses into a root object
+        data.put("courses", courses);
+
+        // Writes to database.json - any existing file will be overwritten
+        FileWriter file = new FileWriter(filePath);
+        file.write(data.toJSONString());
+        file.flush();
+
     }
 
     public static Document loadXMLFromString(String xml) throws Exception {
@@ -49,61 +63,53 @@ public class ExportDataToJson {
     }
 
     static JSONObject createJSONObject(NodeList nList) {
+        /* This class takes a NodeList and retrieves the relevant attributes
+         * from its elements. It structures the data and saves it in a JSONObject. */
+
         JSONObject course = new JSONObject();
         for (int temp = 0; temp < nList.getLength(); temp++) {
-
             Node nNode = nList.item(temp);
 
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) nNode;
 
                 course.put("courseCode", eElement.getAttribute("CourseCode"));
-//                System.out.println("courseCode : " + course.get("courseCode"));
 
                 course.put("danishTitle", eElement.getElementsByTagName("Title")
                         .item(0).getAttributes().getNamedItem("Title").getTextContent());
-//                System.out.println("danishTitle : " + course.get("danishTitle"));
 
                 course.put("englishTitle", eElement.getElementsByTagName("Title")
                         .item(1).getAttributes().getNamedItem("Title").getTextContent());
-//                System.out.println("englishTitle : " + course.get("englishTitle"));
 
-                course.put("ects", eElement.getElementsByTagName("Point").item(0).getTextContent());
-//                System.out.println("ects : " + course.get("ects"));
+                course.put("ects", eElement.getElementsByTagName("Point")
+                        .item(0).getTextContent());
 
                 course.put("courseType", eElement.getElementsByTagName("CBS_Programme_Level")
                         .item(0).getAttributes().getNamedItem("CBS_Programme_LevellKey").getTextContent());
-//                System.out.println("courseType : " + course.get("courseType"));
+
+                course.put("location", eElement.getElementsByTagName("Location")
+                        .item(0).getAttributes().getNamedItem("Key").getTextContent());
 
                 course.put("teachingLanguage", eElement.getElementsByTagName("Teaching_Language")
                         .item(0).getAttributes().getNamedItem("LangCode").getTextContent());
-//                System.out.println("teachingLanguage: " + course.get("teachingLanguage"));
 
                 course.put("mainDepartment", eElement.getElementsByTagName("Main_Dep")
                         .item(0).getAttributes().getNamedItem("UID").getTextContent());
-//                System.out.println("mainDepartment: " + course.get("mainDepartment"));
 
                 course.put("schedule", getSchedule(eElement));
-//                System.out.println("schedulePlacement : " + course.get("schedule").toString());
 
                 course.put("noCreditPointsWith", getNoPointsWith(eElement));
-//                System.out.println("noCreditPointsWith : " + course.get("noCreditPointsWith").toString());
-
 
                 course.put("qualifiedPrerequisites", getPrerequisites(eElement, "Qualified_Prerequisites"));
-//                System.out.println("qualifiedPrerequisites: " + course.get("qualifiedPrerequisites").toString());
 
                 course.put("mandatoryPrerequisites", getPrerequisites(eElement, "Mandatory_Prerequisites"));
-//                System.out.println("mandatoryPrerequisites: " + course.get("mandatoryPrerequisites").toString());
 
                 course.put("previousCourses", getPreviousCourses(eElement));
-//                System.out.println("previousCourses: " + course.get("previousCourses").toString());
 
                 course.put("danishContents", getContents(eElement, "da-DK"));
-//                System.out.println("danishContents: " + course.get("danishContents"));
 
                 course.put("englishContents", getContents(eElement, "en-GB"));
-//                System.out.println("englishContents: " + course.get("englishContents"));
+
                 System.out.println(course.get("courseCode") + " Er nu hentet");
             }
         }
@@ -111,6 +117,7 @@ public class ExportDataToJson {
     }
 
     static String getContents(Element element, String language) {
+        // Returns a short course description in the specified language
         String content = null;
 
         try {
@@ -125,23 +132,25 @@ public class ExportDataToJson {
     }
 
     static JSONArray getPreviousCourses(Element element) {
+        /* Gets a list of previous courses, if any, corresponding to the course,
+         * since courses have gone by different names/course codes in the past. */
+
         JSONArray jsonArray = new JSONArray();
 
         try {
             NodeList courseList = element.getElementsByTagName("PreviousCourse");
             for (int i = 0; i < courseList.getLength(); i++) {
-                jsonArray.put(courseList.item(i).getAttributes().getNamedItem("CourseCode").getTextContent());
+                jsonArray.add(courseList.item(i).getAttributes().getNamedItem("CourseCode").getTextContent());
             }
-        } catch (Exception e) {
-        }
+        } catch (Exception e) {}
+
         return jsonArray;
     }
 
     static JSONArray getSchedule(Element element) {
-        // Returns a JSONArray of schedule placements
-        // If course has several possible placements, e.g. "E4b and January" OR "F4B and August"
-        // Then the array will contain two subarrays: [["E4B","January"],["F4B","August"]]
-
+        /* Returns a JSONArray of schedule placements
+         If course has several possible placements, e.g. "E4b and January" OR "F4B and August"
+         Then the array will contain two subarrays: [["E4B","January"],["F4B","August"]] */
         JSONArray schedule = new JSONArray();
 
         try {
@@ -149,21 +158,26 @@ public class ExportDataToJson {
             for (int i = 0; i < scheduleList.getLength(); i++) {
                 JSONArray scheduleArray = new JSONArray();
                 Element scheduleElement = (Element) scheduleList.item(i);
-                NodeList scheduleKey = scheduleElement.getElementsByTagName("Schedule");
-                for (int j = 0; j < scheduleKey.getLength(); j++) {
-                    String scheduleString = scheduleKey.item(j).getAttributes().getNamedItem("ScheduleKey").getTextContent();
-                    scheduleArray.put(scheduleString);
+                NodeList nodeList = scheduleElement.getElementsByTagName("Schedule");
+
+                for (int j = 0; j < nodeList.getLength(); j++) {
+                    // Puts the schedule key into the schedule array
+                    // E.g. if a course has the schedule "E4B and E2", then scheduleArray = ["E4B","E2"]
+
+                    String scheduleString = nodeList.item(j).getAttributes().getNamedItem("ScheduleKey").getTextContent();
+                    scheduleArray.add(scheduleString);
                 }
-                schedule.put(scheduleArray);
+
+                schedule.add(scheduleArray);
             }
-        } catch (Exception e) {
-        }
+        } catch (Exception e) {}
+
         return schedule;
     }
 
     static JSONArray getNoPointsWith(Element element) {
+        // Returns an array of courses that block for ECTS points for the course (pointspaerring)
         JSONArray noCreditPointsWith = new JSONArray();
-
 
         try {
             String noPoints = element.getElementsByTagName("No_Credit_Points_With")
@@ -171,46 +185,65 @@ public class ExportDataToJson {
             Pattern pattern = Pattern.compile("\\d{5}");
             Matcher matcher = pattern.matcher(noPoints);
             while (matcher.find()) {
-                noCreditPointsWith.put(matcher.group());
+                noCreditPointsWith.add(matcher.group());
             }
-        } catch (Exception e) {
-        }
+        } catch (Exception e) {}
+
         return noCreditPointsWith;
     }
 
     static JSONArray getPrerequisites(Element element, String type) {
-        // Qualified prerequisites:
-        // [["02101","02102","02312"],[["02105"]],["01017","01019"]]
+        /* Returns a list of prerequisites, determined by the string type.
+         In the API, the "DTU_CoursesTxt" contains prerequisites in a specific format,
+         e.g. "01017/01019.01005/01006/01015/01016": This means that 01017 OR 01019 are required,
+         and that 01005 OR 01006 OR 01015 OR 01016 are required.
 
-        JSONArray qualifiedPrerequisites = new JSONArray();
+         In other words, "." means "AND", and "/" means "OR".
+
+         In this method, the above requirements would be reformatted as:
+         [["01017","01019"],["01005","01006","01015","01016"]].
+         */
+
+
+        JSONArray prerequisites = new JSONArray();
 
         try {
-
             NodeList qualifiedPrerequisitesList = element.getElementsByTagName(type);
             Element qualifiedElement = (Element) qualifiedPrerequisitesList.item(0);
-            NodeList comeon = qualifiedElement.getElementsByTagName("DTU_CoursesTxt");
+            NodeList nodeList = qualifiedElement.getElementsByTagName("DTU_CoursesTxt");
 
-            String prerequisitesString = comeon
+            // Get the prerequisites in string form
+            String prerequisitesString = nodeList
                     .item(0).getAttributes()
                     .getNamedItem("Txt").getTextContent();
 
+            // Split the prerequisites by "AND" first
             String[] prerequisitesStringSplit = prerequisitesString
                     .replace("(", "")
                     .replace(")", "")
-                    .split("[\\s.,]+");
+                    //.split("[\\s.,]+");
+                    .split("\\s*(\\.|\\s)\\s*");
 
             for (int i = 0; i < prerequisitesStringSplit.length; i++) {
+                // Split the prerequisites by "OR"
+
+                JSONArray prerequisite = new JSONArray();
                 if (!prerequisitesStringSplit[i].matches(".*[a-zA-Z]+.*")) {
-                    // TODO: Trim så mellemrum ikke kommer med, på en smart måde
-                    qualifiedPrerequisites.put(prerequisitesStringSplit[i].trim().split("[/]"));
+                    String[] array = (prerequisitesStringSplit[i].trim().split("[/]"));
+                    for (int j = 0; j < array.length; j++) {
+                        prerequisite.add(array[j]);
+                    }
+
+                    prerequisites.add(prerequisite);
                 }
             }
-        } catch (Exception e) {
-        }
-        return qualifiedPrerequisites;
+        } catch (Exception e) {}
+
+        return prerequisites;
     }
 
     static String getCourse(String id) throws Exception {
+        // Returns an XML string of the course information from the API
 
         String url = "https://kurser.dtu.dk/coursewebservicev2/course.asmx/GetCourse?courseCode=" + id + "&yearGroup=2019/2020";
 
@@ -219,7 +252,7 @@ public class ExportDataToJson {
         TrustModifier.relaxHostChecking(con);
         con.setRequestMethod("GET");
 
-        //add request header
+        // Add request header
         con.setRequestProperty("User-Agent", USER_AGENT);
 
         int responseCode = con.getResponseCode();
@@ -235,8 +268,8 @@ public class ExportDataToJson {
             response.append(inputLine);
         }
         in.close();
+        in.close();
 
-        //print result
         return response.toString();
     }
 
