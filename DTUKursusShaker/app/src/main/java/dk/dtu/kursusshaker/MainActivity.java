25f91ff8f646ac187714381cf02e48f19353ffbc
Sigurd <sigurd.thorlund@gmail.com>
2019-06-18 09:55:29 +0200

    onBoarding start

--- DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/MainActivity.java
+++ DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/MainActivity.java
@@ -1,14 +1,20 @@
 package dk.dtu.kursusshaker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import dk.dtu.kursusshaker.activities.IntroActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private Button homeButton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,6 +43,18 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, IntroActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
+
 
 }
