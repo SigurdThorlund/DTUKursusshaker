f32516f520d9312ec95d2b262984890dcac7593d
Mikkel Anderson <s184230@student.dtu.dk>
2019-06-18 13:51:59 +0200

    Auto stash before merge of "feature/menus-and-navigation" and "DTU-kurser/feature/menus-and-navigation"

--- DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/DashboardFragment.java
+++ DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/DashboardFragment.java
@@ -1,16 +1,24 @@
 package dk.dtu.kursusshaker.fragments;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import dk.dtu.kursusshaker.R;
 
@@ -27,10 +35,13 @@ public class DashboardFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    View view;
+    SimpleAdapter simpleAdapter;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -65,11 +76,63 @@ public class DashboardFragment extends Fragment {
         }
     }
 
+    private void insertCoursesInListView() { //TODO skal laves til MVC
+        String[] courseNames = {"Matematik 1", "Matematik 2", "Statistik", "Software", "Android"};
+        String[] courseIds = {"01005", "01839", "32892", "09732", "56782"};
+
+        ArrayList<Map<String, Object>> itemDataList = new ArrayList<Map<String, Object>>();
+
+        int titleLen = courseNames.length;
+        for (int i = 0; i < titleLen; i++) {
+            Map<String, Object> listItemMap = new HashMap<String, Object>();
+            listItemMap.put("title", courseNames[i]);
+            listItemMap.put("description", courseIds[i]);
+            itemDataList.add(listItemMap);
+        }
+
+        simpleAdapter = new SimpleAdapter(getActivity(), itemDataList, android.R.layout.simple_list_item_2,
+                new String[]{"title", "description"}, new int[]{android.R.id.text1, android.R.id.text2});
+
+        ListView listView = (ListView) view.findViewById(R.id.list_item_all_courses);
+        listView.setAdapter(simpleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
+                Object clickItemObj = adapterView.getAdapter().getItem(index);
+                //     Toast.makeText(this, "You clicked " + clickItemObj.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextInputEditText courseFilter = (TextInputEditText) view.findViewById(R.id.search_all_courses_filter);
+
+        courseFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                simpleAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        this.insertCoursesInListView();
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -110,4 +173,5 @@ public class DashboardFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
