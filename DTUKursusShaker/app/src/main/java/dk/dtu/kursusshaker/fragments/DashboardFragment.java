5976b75b5d948245d6fb06283b2fd5cfad550849
Benjamin Starostka Jakobsen <pluffy.jakobsen@gmail.com>
2019-06-19 18:07:11 +0200

    Moved what was on dashboard to search to test new design

--- DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/DashboardFragment.java
+++ DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/DashboardFragment.java
@@ -44,10 +44,6 @@ public class DashboardFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    View view;
-    SimpleAdapter simpleAdapter;
-
-
     private OnFragmentInteractionListener mListener;
 
     public DashboardFragment() {
@@ -81,76 +77,14 @@ public class DashboardFragment extends Fragment {
         }
     }
 
-    private void insertCoursesInListView() throws IOException { //TODO skal laves til MVC
-        CoursesAsObject coursesAsObject = new CoursesAsObject(getContext());
-        Course[] course = coursesAsObject.getCourseArray();
 
-        String[] courseNames = new String[course.length];
-        String[] courseIds = new String[course.length];
-        // String[] courseNames = {"Matematik 1", "Matematik 2", "Statistik", "Software", "Android"};
-        // String[] courseIds = {"01005", "01839", "32892", "09732", "56782"};
-        for (int i = 0; i < course.length; i++) {
-            courseNames[i] = course[i].getDanishTitle();
-            courseIds[i] = course[i].getCourseCode();
-        }
-
-        ArrayList<Map<String, Object>> itemDataList = new ArrayList<Map<String, Object>>();
-
-        int titleLen = courseNames.length;
-        for (int i = 0; i < titleLen; i++) {
-            Map<String, Object> listItemMap = new HashMap<String, Object>();
-            listItemMap.put("title", courseNames[i]);
-            listItemMap.put("description", courseIds[i]);
-            itemDataList.add(listItemMap);
-        }
-
-        simpleAdapter = new SimpleAdapter(getActivity(), itemDataList, android.R.layout.simple_list_item_2,
-                new String[]{"title", "description"}, new int[]{android.R.id.text1, android.R.id.text2});
-
-        ListView listView = (ListView) view.findViewById(R.id.list_item_all_courses);
-        listView.setAdapter(simpleAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
-                Object clickItemObj = adapterView.getAdapter().getItem(index);
-                //     Toast.makeText(this, "You clicked " + clickItemObj.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        TextInputEditText courseFilter = (TextInputEditText) view.findViewById(R.id.search_all_courses_filter);
-
-        courseFilter.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                simpleAdapter.getFilter().filter(s);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        try {
-            this.insertCoursesInListView();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return view;
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/SearchFragment.java
+++ DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/SearchFragment.java
@@ -6,11 +6,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import dk.dtu.kursusshaker.R;
+import dk.dtu.kursusshaker.data.Course;
+import dk.dtu.kursusshaker.data.CoursesAsObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +43,9 @@ public class SearchFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    View view;
+    SimpleAdapter simpleAdapter;
 
     private OnFragmentInteractionListener mListener;
 
@@ -63,11 +80,75 @@ public class SearchFragment extends Fragment {
         }
     }
 
+    private void insertCoursesInListView() throws IOException { //TODO skal laves til MVC
+        CoursesAsObject coursesAsObject = new CoursesAsObject(getContext());
+        Course[] course = coursesAsObject.getCourseArray();
+
+        String[] courseNames = new String[course.length];
+        String[] courseIds = new String[course.length];
+        // String[] courseNames = {"Matematik 1", "Matematik 2", "Statistik", "Software", "Android"};
+        // String[] courseIds = {"01005", "01839", "32892", "09732", "56782"};
+        for (int i = 0; i < course.length; i++) {
+            courseNames[i] = course[i].getDanishTitle();
+            courseIds[i] = course[i].getCourseCode();
+        }
+
+        ArrayList<Map<String, Object>> itemDataList = new ArrayList<Map<String, Object>>();
+
+        int titleLen = courseNames.length;
+        for (int i = 0; i < titleLen; i++) {
+            Map<String, Object> listItemMap = new HashMap<String, Object>();
+            listItemMap.put("title", courseNames[i]);
+            listItemMap.put("description", courseIds[i]);
+            itemDataList.add(listItemMap);
+        }
+
+        simpleAdapter = new SimpleAdapter(getActivity(), itemDataList, android.R.layout.simple_list_item_2,
+                new String[]{"title", "description"}, new int[]{android.R.id.text1, android.R.id.text2});
+
+        ListView listView = (ListView) view.findViewById(R.id.list_item_all_courses);
+        listView.setAdapter(simpleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
+                Object clickItemObj = adapterView.getAdapter().getItem(index);
+                //     Toast.makeText(this, "You clicked " + clickItemObj.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextInputEditText courseFilter = (TextInputEditText) view.findViewById(R.id.search_all_courses_filter);
+
+        courseFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                simpleAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        view = inflater.inflate(R.layout.fragment_search, container, false);
+        try {
+            this.insertCoursesInListView();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
