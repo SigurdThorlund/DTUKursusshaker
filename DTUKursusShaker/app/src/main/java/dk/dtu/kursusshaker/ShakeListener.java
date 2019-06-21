49e45a868588d5e2063a5c80f6b49cd203eb1f50
Unfunctioned <bjwil@live.dk>
2019-06-21 17:36:26 +0200

    Minor changes

--- DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/ShakeListener.java
+++ DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/ShakeListener.java
@@ -5,74 +5,36 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
 public class ShakeListener implements SensorEventListener {
-    // Minimum acceleration needed to count as a shake movement
-    private static final int MIN_SHAKE_ACCELERATION = 5;
-
-    // Minimum number of movements to register a shake
-    private static final int MIN_MOVEMENTS = 2;
-
-    // Maximum time (in milliseconds) for the whole shake to occur
-    private static final int MAX_SHAKE_DURATION = 500;
-
-    // Arrays to store gravity and linear acceleration values
-    private float[] mGravity = { 0.0f, 0.0f, 0.0f };
-    private float[] mLinearAcceleration = { 0.0f, 0.0f, 0.0f };
-
-    // Indexes for x, y, and z values
-    private static final int X = 0;
-    private static final int Y = 1;
-    private static final int Z = 2;
-
-    // OnShakeListener that will be notified when the shake is detected
+    private static final int MIN_SHAKE_STENGHT = 5; //Minimum acceleration need to register a shake
+    private static final int MIN_MOVEMENTS = 2; //The minimum amount of movements needed to register a shake
+    private static final int MAX_SHAKE_TIME = 500; //The maximum time a shake needs to last inorder to be registered
+    private float[] gravity = { 0.0f, 0.0f, 0.0f };
+    private float[] linearAcceleration = { 0.0f, 0.0f, 0.0f };
     private OnShakeListener onShakeListener;
-
-    // Start time for the shake detection
     long startTime = 0;
-
-    // Counter for shake movements
     int moveCount = 0;
-
-    // Constructor that sets the shake listener
     public ShakeListener(OnShakeListener shakeListener) {
         onShakeListener = shakeListener;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        // This method will be called when the accelerometer detects a change.
-
-        // Call a helper method that wraps code from the Android developer site
         setCurrentAcceleration(event);
-
-        // Get the max linear acceleration in any direction
         float maxLinearAcceleration = getMaxCurrentLinearAcceleration();
-
-        // Check if the acceleration is greater than our minimum threshold
-        if (maxLinearAcceleration > MIN_SHAKE_ACCELERATION) {
+        if (maxLinearAcceleration > MIN_SHAKE_STENGHT) {
             long now = System.currentTimeMillis();
-
-            // Set the startTime if it was reset to zero
             if (startTime == 0) {
                 startTime = now;
             }
-
             long elapsedTime = now - startTime;
-
-            // Check if we're still in the shake window we defined
-            if (elapsedTime > MAX_SHAKE_DURATION) {
-                // Too much time has passed. Start over!
+            if (elapsedTime > MAX_SHAKE_TIME) {
                 resetShakeDetection();
             }
             else {
-                // Keep track of all the movements
                 moveCount++;
 
-                // Check if enough movements have been made to qualify as a shake
                 if (moveCount > MIN_MOVEMENTS) {
-                    // It's a shake! Notify the listener.
                     onShakeListener.onShake();
-
-                    // Reset for the next one!
                     resetShakeDetection();
                 }
             }
@@ -81,50 +43,35 @@ public class ShakeListener implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Intentionally blank
+        //Unused interface method, needs to be kept though
+        //since interface demands implementation
     }
 
     private void setCurrentAcceleration(SensorEvent event) {
-        /*
-         *  BEGIN SECTION from Android developer site. This code accounts for
-         *  gravity using a high-pass filter
-         */
-
-        // alpha is calculated as t / (t + dT)
-        // with t, the low-pass filter's time-constant
-        // and dT, the event delivery rate
-
         final float alpha = 0.8f;
 
         // Gravity components of x, y, and z acceleration
-        mGravity[X] = alpha * mGravity[X] + (1 - alpha) * event.values[X];
-        mGravity[Y] = alpha * mGravity[Y] + (1 - alpha) * event.values[Y];
-        mGravity[Z] = alpha * mGravity[Z] + (1 - alpha) * event.values[Z];
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
 
         // Linear acceleration along the x, y, and z axes (gravity effects removed)
-        mLinearAcceleration[X] = event.values[X] - mGravity[X];
-        mLinearAcceleration[Y] = event.values[Y] - mGravity[Y];
-        mLinearAcceleration[Z] = event.values[Z] - mGravity[Z];
-
-        /*
-         *  END SECTION from Android developer site
-         */
+        linearAcceleration[0] = event.values[0] - gravity[0];
+        linearAcceleration[1] = event.values[1] - gravity[1];
+        linearAcceleration[2] = event.values[2] - gravity[2];
     }
 
     private float getMaxCurrentLinearAcceleration() {
         // Start by setting the value to the x value
-        float maxLinearAcceleration = mLinearAcceleration[X];
-
+        float maxLinearAcceleration = linearAcceleration[0];
         // Check if the y value is greater
-        if (mLinearAcceleration[Y] > maxLinearAcceleration) {
-            maxLinearAcceleration = mLinearAcceleration[Y];
+        if (linearAcceleration[1] > maxLinearAcceleration) {
+            maxLinearAcceleration = linearAcceleration[1];
         }
-
         // Check if the z value is greater
-        if (mLinearAcceleration[Z] > maxLinearAcceleration) {
-            maxLinearAcceleration = mLinearAcceleration[Z];
+        if (linearAcceleration[2] > maxLinearAcceleration) {
+            maxLinearAcceleration = linearAcceleration[2];
         }
-
         // Return the greatest value
         return maxLinearAcceleration;
     }
