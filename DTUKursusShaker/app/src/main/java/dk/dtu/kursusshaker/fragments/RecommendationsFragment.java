d94b32c831ae16436039f62304d31db2cdda7a97
Benjamin <pluffy.jakobsen@gmail.com>
2019-06-25 00:22:33 +0200

    Add recommended to basket

--- DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/RecommendationsFragment.java
+++ DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/RecommendationsFragment.java
@@ -1,16 +1,27 @@
 package dk.dtu.kursusshaker.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.HashSet;
 
 import dk.dtu.kursusshaker.R;
+import dk.dtu.kursusshaker.data.Course;
 import dk.dtu.kursusshaker.data.PrimaryViewModel;
 import dk.dtu.kursusshaker.fragments.dummy.DummyContent.DummyItem;
 
@@ -25,6 +36,9 @@ public class RecommendationsFragment extends Fragment {
     private PrimaryViewModel primaryViewModel;
 
     private OnListFragmentInteractionListener mListener;
+
+    HashSet<String> takenCourses;
+    SharedPreferences sp;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -42,11 +56,41 @@ public class RecommendationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        sp = getActivity().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        takenCourses = (HashSet<String>) sp.getStringSet("Courses", new HashSet<String>());
+
         primaryViewModel = ViewModelProviders.of(getActivity()).get(PrimaryViewModel.class);
+        final Course recommendedCourse = primaryViewModel.getRecommendedCourse();
+
         View view = inflater.inflate(R.layout.fragment_recommendation, container, false);
 
         TextView recommendedCourseTitleView = view.findViewById(R.id.recommendedCourseTitle);
-        recommendedCourseTitleView.setText(primaryViewModel.getRecommendedCourse().getDanishTitle());
+        recommendedCourseTitleView.setText(recommendedCourse.getDanishTitle());
+
+        TextView recommendedCourseID = view.findViewById(R.id.recommendedCourseNumber);
+        recommendedCourseID.setText(recommendedCourse.getCourseCode());
+
+        TextView recommendedCourseDescription = view.findViewById(R.id.recommendedCourseDescription);
+        recommendedCourseDescription.setText(recommendedCourse.getDanishContents());
+
+        // Also add the button as well as an listener
+        Button addToBasketButton = view.findViewById(R.id.recommendedCourseAddToBasketButton);
+        addToBasketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (takenCourses.contains(recommendedCourse.getCourseCode())) {
+                    Toast toast = Toast.makeText(getContext(), "Du har allerede tilføjet dette kursus", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL,0,0);
+                    toast.show();
+                } else {
+                    takenCourses.add(recommendedCourse.getCourseCode());
+                    Toast toast = Toast.makeText(getContext(), recommendedCourse.getDanishTitle() + " tilføjet til kurven!", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL,0,0);
+                    toast.show();
+                }
+                sp.edit().putStringSet("Courses", takenCourses).apply();
+            }
+        });
 
         return view;
     }
