dc5531eda51bf6edd90ccd52a437a4af9a1c6fbb
Sigurd <sigurd.thorlund@gmail.com>
2019-06-24 22:47:45 +0200

    Put Reset prefs in settings fragment

--- DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/TagetKursusFragment.java
+++ DTUKursusShaker/app/src/main/java/dk/dtu/kursusshaker/fragments/TagetKursusFragment.java
@@ -30,7 +30,6 @@ public class TagetKursusFragment extends OnboardingFragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
